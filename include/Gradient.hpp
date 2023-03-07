d08a21a68f20061ff24923a029f7089a6e771859
tangkai <tangkai@cvte.com>
2023-03-07 10:05:47 +0800

    新文件：   .gitignore 	修改：     .vscode/c_cpp_properties.json 	新文件：   .vscode/settings.json 	修改：     CMakeLists.txt 	修改：     include/Gradient.hpp 	修改：     include/Utility.hpp

--- include/Gradient.hpp
+++ include/Gradient.hpp
@@ -32,7 +32,7 @@ public:
     GradientOperator() = delete;
 
     GradientOperator( int width, int height ) :
-        ImgWidth( width ), ImgHeight( height )
+        width_( width ), height_( height )
     {
         Initialize();
     }
@@ -45,22 +45,22 @@ public:
 
     short *GetPtrDx() const
     {
-        return pImgDx;
+        return pImgDx_;
     }
 
     short *GetPtrDy() const
     {
-        return pImgDy;
+        return pImgDy_;
     }
 
     short *GetPtrGrad() const
     {
-        return pImgGrad;
+        return pImgGrad_;
     }
 
     bool IsHorizontal( uint index ) const
     {
-        return Abs( pImgDx[index] ) < Abs( pImgDy[index] );
+        return Abs( pImgDx_[index] ) < Abs( pImgDy_[index] );
     }
 
 private:
@@ -82,47 +82,47 @@ private:
 
 private:
 
-    const int ImgWidth = 1280;
-    const int ImgHeight = 720;
+    const int width_ = 1280;
+    const int height_ = 720;
 
 private:
 
-    uchar *raw = nullptr;
-    short *dx_dy_grad = nullptr;
+    uchar *raw_ = nullptr;
+    short *dx_dy_grad_ = nullptr;
 
-    short *pImgDx = nullptr;
-    short *pImgDy = nullptr;
-    short *pImgGrad = nullptr;
+    short *pImgDx_ = nullptr;
+    short *pImgDy_ = nullptr;
+    short *pImgGrad_ = nullptr;
 };
 
 
 void GradientOperator::Initialize()
 {
-    raw = new uchar[ImgWidth * 3];
+    raw_ = new uchar[width_ * 3];
 
-    dx_dy_grad = new short[ImgWidth * 3];
-    memset( dx_dy_grad, 0, ImgWidth * sizeof( short ) * 3 );
+    dx_dy_grad_ = new short[width_ * 3];
+    memset( dx_dy_grad_, 0, width_ * sizeof( short ) * 3 );
 
 
-    const int pixelsNum = ImgWidth * ImgHeight;
-    pImgDx = new short[pixelsNum];
-    pImgDy = new short[pixelsNum];
-    pImgGrad = new short[pixelsNum];
+    const int pixelsNum = width_ * height_;
+    pImgDx_ = new short[pixelsNum];
+    pImgDy_ = new short[pixelsNum];
+    pImgGrad_ = new short[pixelsNum];
 
-    memset( pImgDx, 0, pixelsNum * sizeof( short ) );
-    memset( pImgDy, 0, pixelsNum * sizeof( short ) );
-    memset( pImgGrad, 0, pixelsNum * sizeof( short ) );
+    memset( pImgDx_, 0, pixelsNum * sizeof( short ) );
+    memset( pImgDy_, 0, pixelsNum * sizeof( short ) );
+    memset( pImgGrad_, 0, pixelsNum * sizeof( short ) );
 }
 
 
 GradientOperator::~GradientOperator()
 {
-    delete []raw;
-    delete []dx_dy_grad;
+    delete []raw_;
+    delete []dx_dy_grad_;
 
-    delete []pImgDx;
-    delete []pImgDy;
-    delete []pImgGrad;
+    delete []pImgDx_;
+    delete []pImgDy_;
+    delete []pImgGrad_;
 }
 
 
@@ -157,72 +157,72 @@ void GradientOperator::Compute( const uchar *pImg )
 void GradientOperator::Compute_AVX( const uchar *pImg )
 {
     const int blockSize_AVX = 16;
-    const int area_AVX = ImgWidth - ( ImgWidth - 1 ) % blockSize_AVX;
-    const int widthBytesShort = ImgWidth * sizeof( short );
+    const int area_AVX = width_ - ( width_ - 1 ) % blockSize_AVX;
+    const int widthBytesShort = width_ * sizeof( short );
 
-    uchar *up = raw;
-    uchar *mid = raw + ImgWidth;
-    uchar *down = raw + ImgWidth * 2;
+    uchar *u = raw_;                  //up
+    uchar *m = raw_ + width_;         //mid
+    uchar *d = raw_ + width_ * 2;     //down
 
-    short *dx = dx_dy_grad;
-    short *dy = dx_dy_grad + ImgWidth;
-    short *grad = dx_dy_grad + ImgWidth * 2;
+    short *dx = dx_dy_grad_;
+    short *dy = dx_dy_grad_ + width_;
+    short *g = dx_dy_grad_ + width_ * 2;
 
-    for ( int y = 1; y < ImgHeight - 1; ++y )
+    for ( int y = 1; y < height_ - 1; ++y )
     {
         if ( y == 1 )
         {
-            memcpy( raw, pImg, ImgWidth * 3 );
+            memcpy( raw_, pImg, width_ * 3 );
         }
         else
         {
-            uchar *temp = up;
-            up = mid;
-            mid = down;
-            down = temp;
+            uchar *temp = u;
+            u = m;
+            m = d;
+            d = temp;
 
-            memcpy( down, pImg + ImgWidth * ( y + 1 ), ImgWidth );
+            memcpy( d, pImg + width_ * ( y + 1 ), width_ );
         }
 
         for ( int x = 1; x < area_AVX; x += blockSize_AVX )
         {
-            __m256i up0 = _mm256_cvtepu8_epi16( _mm_loadu_si128( ( const __m128i * )( up + x - 1 ) ) );
-            __m256i up1 = _mm256_slli_epi16( _mm256_cvtepu8_epi16( _mm_loadu_si128( ( const __m128i * )( up + x ) ) ), 1 );
-            __m256i up2 = _mm256_cvtepu8_epi16( _mm_loadu_si128( ( const __m128i * )( up + x + 1 ) ) );
+            __m256i u0 = _mm256_cvtepu8_epi16( _mm_loadu_si128( ( const __m128i * )( u + x - 1 ) ) );
+            __m256i u1 = _mm256_slli_epi16( _mm256_cvtepu8_epi16( _mm_loadu_si128( ( const __m128i * )( u + x ) ) ), 1 );
+            __m256i u2 = _mm256_cvtepu8_epi16( _mm_loadu_si128( ( const __m128i * )( u + x + 1 ) ) );
 
-            __m256i mid0 = _mm256_slli_epi16( _mm256_cvtepu8_epi16( _mm_loadu_si128( ( const __m128i * )( mid + x - 1 ) ) ), 1 );
-            __m256i mid2 = _mm256_slli_epi16( _mm256_cvtepu8_epi16( _mm_loadu_si128( ( const __m128i * )( mid + x + 1 ) ) ), 1 );
+            __m256i m0 = _mm256_slli_epi16( _mm256_cvtepu8_epi16( _mm_loadu_si128( ( const __m128i * )( m + x - 1 ) ) ), 1 );
+            __m256i m2 = _mm256_slli_epi16( _mm256_cvtepu8_epi16( _mm_loadu_si128( ( const __m128i * )( m + x + 1 ) ) ), 1 );
 
-            __m256i down0 = _mm256_cvtepu8_epi16( _mm_loadu_si128( ( const __m128i * )( down + x - 1 ) ) );
-            __m256i down1 = _mm256_slli_epi16( _mm256_cvtepu8_epi16( _mm_loadu_si128( ( const __m128i * )( down + x ) ) ), 1 );
-            __m256i down2 = _mm256_cvtepu8_epi16( _mm_loadu_si128( ( const __m128i * )( down + x + 1 ) ) );
+            __m256i d0 = _mm256_cvtepu8_epi16( _mm_loadu_si128( ( const __m128i * )( d + x - 1 ) ) );
+            __m256i d1 = _mm256_slli_epi16( _mm256_cvtepu8_epi16( _mm_loadu_si128( ( const __m128i * )( d + x ) ) ), 1 );
+            __m256i d2 = _mm256_cvtepu8_epi16( _mm_loadu_si128( ( const __m128i * )( d + x + 1 ) ) );
 
 
-            __m256i dx16 = _mm256_subs_epi16( _mm256_adds_epi16( _mm256_adds_epi16( up2, mid2 ), down2 ),
-                                              _mm256_adds_epi16( _mm256_adds_epi16( up0, mid0 ), down0 ) );
+            __m256i dx16 = _mm256_subs_epi16( _mm256_adds_epi16( _mm256_adds_epi16( u2, m2 ), d2 ),
+                                              _mm256_adds_epi16( _mm256_adds_epi16( u0, m0 ), d0 ) );
 
-            __m256i dy16 = _mm256_subs_epi16( _mm256_adds_epi16( _mm256_adds_epi16( down0, down1 ), down2 ),
-                                              _mm256_adds_epi16( _mm256_adds_epi16( up0, up1 ), up2 ) );
+            __m256i dy16 = _mm256_subs_epi16( _mm256_adds_epi16( _mm256_adds_epi16( d0, d1 ), d2 ),
+                                              _mm256_adds_epi16( _mm256_adds_epi16( u0, u1 ), u2 ) );
 
             __m256i g16 = _mm256_srai_epi16( _mm256_adds_epi16( _mm256_abs_epi16( dx16 ), _mm256_abs_epi16( dy16 ) ), 2 );
 
 
             _mm256_storeu_si256( ( __m256i * )( dx + x ), dx16 );
             _mm256_storeu_si256( ( __m256i * )( dy + x ), dy16 );
-            _mm256_storeu_si256( ( __m256i * )( grad + x ), g16 );
+            _mm256_storeu_si256( ( __m256i * )( g + x ), g16 );
         }
 
 
-        for ( int x = area_AVX; x < ImgWidth - 1; ++x )
+        for ( int x = area_AVX; x < width_ - 1; ++x )
         {
-            dx[x] = up[x + 1] - up[x - 1] + ( mid[x + 1] - mid[x - 1] ) * 2 + down[x + 1] - down[x - 1];
-            dy[x] = down[x - 1] + down[x + 1] + ( down[x] - up[x] ) * 2 - up[x - 1] - up[x + 1];
-            grad[x] = ( Abs( dx[x] ) + Abs( dy[x] ) ) >> 2;
+            dx[x] = u[x + 1] - u[x - 1] + ( m[x + 1] - m[x - 1] ) * 2 + d[x + 1] - d[x - 1];
+            dy[x] = d[x - 1] + d[x + 1] + ( d[x] - u[x] ) * 2 - u[x - 1] - u[x + 1];
+            g[x] = ( Abs( dx[x] ) + Abs( dy[x] ) ) >> 2;
         }
 
-        memcpy( pImgDx + y * ImgWidth, dx, widthBytesShort );
-        memcpy( pImgDy + y * ImgWidth, dy, widthBytesShort );
-        memcpy( pImgGrad + y * ImgWidth, grad, widthBytesShort );
+        memcpy( pImgDx_ + y * width_, dx, widthBytesShort );
+        memcpy( pImgDy_ + y * width_, dy, widthBytesShort );
+        memcpy( pImgGrad_ + y * width_, g, widthBytesShort );
     }
 }
 #endif
@@ -232,72 +232,72 @@ void GradientOperator::Compute_AVX( const uchar *pImg )
 void GradientOperator::Compute_SSE( const uchar *pImg )
 {
     const int blockSize_SSE = 8;
-    const int area_SSE = ImgWidth - ( ImgWidth - 1 ) % blockSize_SSE;
-    const int widthBytesShort = ImgWidth * sizeof( short );
+    const int area_SSE = width_ - ( width_ - 1 ) % blockSize_SSE;
+    const int widthBytesShort = width_ * sizeof( short );
 
-    uchar *up = raw;
-    uchar *mid = raw + ImgWidth;
-    uchar *down = raw + ImgWidth * 2;
+    uchar *u = raw_;
+    uchar *m = raw_ + width_;
+    uchar *d = raw_ + width_ * 2;
 
-    short *dx = dx_dy_grad;
-    short *dy = dx_dy_grad + ImgWidth;
-    short *grad = dx_dy_grad + ImgWidth * 2;
+    short *dx = dx_dy_grad_;
+    short *dy = dx_dy_grad_ + width_;
+    short *g = dx_dy_grad_ + width_ * 2;
 
     __m128i zero = _mm_setzero_si128();
 
-    for ( int y = 1; y < ImgHeight - 1; ++y )
+    for ( int y = 1; y < height_ - 1; ++y )
     {
         if ( y == 1 )
         {
-            memcpy( raw, pImg, ImgWidth * 3 );
+            memcpy( raw_, pImg, width_ * 3 );
         }
         else
         {
-            uchar *temp = up;
-            up = mid;
-            mid = down;
-            down = temp;
+            uchar *temp = u;
+            u = m;
+            m = d;
+            d = temp;
 
-            memcpy( down, pImg + ImgWidth * ( y + 1 ), ImgWidth );
+            memcpy( d, pImg + width_ * ( y + 1 ), width_ );
         }
 
         for ( int x = 1; x < area_SSE; x += blockSize_SSE )
         {
-            __m128i up0 = _mm_unpacklo_epi8( _mm_loadl_epi64( ( __m128i * )( up + x - 1 ) ), zero );
-            __m128i up1 = _mm_slli_epi16( _mm_unpacklo_epi8( _mm_loadl_epi64( ( __m128i * )( up + x ) ), zero ), 1 );
-            __m128i up2 = _mm_unpacklo_epi8( _mm_loadl_epi64( ( __m128i * )( up + x + 1 ) ), zero );
+            __m128i u0 = _mm_unpacklo_epi8( _mm_loadl_epi64( ( __m128i * )( u + x - 1 ) ), zero );
+            __m128i u1 = _mm_slli_epi16( _mm_unpacklo_epi8( _mm_loadl_epi64( ( __m128i * )( u + x ) ), zero ), 1 );
+            __m128i u2 = _mm_unpacklo_epi8( _mm_loadl_epi64( ( __m128i * )( u + x + 1 ) ), zero );
 
-            __m128i mid0 = _mm_slli_epi16( _mm_unpacklo_epi8( _mm_loadl_epi64( ( __m128i * )( mid + x - 1 ) ), zero ), 1 );
-            __m128i mid2 = _mm_slli_epi16( _mm_unpacklo_epi8( _mm_loadl_epi64( ( __m128i * )( mid + x + 1 ) ), zero ), 1 );
+            __m128i m0 = _mm_slli_epi16( _mm_unpacklo_epi8( _mm_loadl_epi64( ( __m128i * )( m + x - 1 ) ), zero ), 1 );
+            __m128i m2 = _mm_slli_epi16( _mm_unpacklo_epi8( _mm_loadl_epi64( ( __m128i * )( m + x + 1 ) ), zero ), 1 );
 
-            __m128i down0 = _mm_unpacklo_epi8( _mm_loadl_epi64( ( __m128i * )( down + x - 1 ) ), zero );
-            __m128i down1 = _mm_slli_epi16( _mm_unpacklo_epi8( _mm_loadl_epi64( ( __m128i * )( down + x ) ), zero ), 1 );
-            __m128i down2 = _mm_unpacklo_epi8( _mm_loadl_epi64( ( __m128i * )( down + x + 1 ) ), zero );
+            __m128i d0 = _mm_unpacklo_epi8( _mm_loadl_epi64( ( __m128i * )( d + x - 1 ) ), zero );
+            __m128i d1 = _mm_slli_epi16( _mm_unpacklo_epi8( _mm_loadl_epi64( ( __m128i * )( d + x ) ), zero ), 1 );
+            __m128i d2 = _mm_unpacklo_epi8( _mm_loadl_epi64( ( __m128i * )( d + x + 1 ) ), zero );
 
 
-            __m128i dx8 = _mm_subs_epi16( _mm_adds_epi16( _mm_adds_epi16( up2, mid2 ), down2 ),
-                                          _mm_adds_epi16( _mm_adds_epi16( up0, mid0 ), down0 ) );
+            __m128i dx8 = _mm_subs_epi16( _mm_adds_epi16( _mm_adds_epi16( u2, m2 ), d2 ),
+                                          _mm_adds_epi16( _mm_adds_epi16( u0, m0 ), d0 ) );
 
-            __m128i dy8 = _mm_subs_epi16( _mm_adds_epi16( _mm_adds_epi16( down0, down1 ), down2 ),
-                                          _mm_adds_epi16( _mm_adds_epi16( up0, up1 ), up2 ) );
+            __m128i dy8 = _mm_subs_epi16( _mm_adds_epi16( _mm_adds_epi16( d0, d1 ), d2 ),
+                                          _mm_adds_epi16( _mm_adds_epi16( u0, u1 ), u2 ) );
 
             __m128i g8 = _mm_srai_epi16( _mm_adds_epi16( _mm_abs_epi16( dx8 ), _mm_abs_epi16( dy8 ) ), 2 );
 
             _mm_storeu_si128( ( __m128i * )( dx + x ), dx8 );
             _mm_storeu_si128( ( __m128i * )( dy + x ), dy8 );
-            _mm_storeu_si128( ( __m128i * )( grad + x ), g8 );
+            _mm_storeu_si128( ( __m128i * )( g + x ), g8 );
         }
 
-        for ( int x = area_SSE; x < ImgWidth - 1; ++x )
+        for ( int x = area_SSE; x < width_ - 1; ++x )
         {
-            dx[x] = up[x + 1] - up[x - 1] + ( mid[x + 1] - mid[x - 1] ) * 2 + down[x + 1] - down[x - 1];
-            dy[x] = down[x - 1] + down[x + 1] + ( down[x] - up[x] ) * 2 - up[x - 1] - up[x + 1];
-            grad[x] = ( Abs( dx[x] ) + Abs( dy[x] ) ) >> 2;
+            dx[x] = u[x + 1] - u[x - 1] + ( m[x + 1] - m[x - 1] ) * 2 + d[x + 1] - d[x - 1];
+            dy[x] = d[x - 1] + d[x + 1] + ( d[x] - u[x] ) * 2 - u[x - 1] - u[x + 1];
+            g[x] = ( Abs( dx[x] ) + Abs( dy[x] ) ) >> 2;
         }
 
-        memcpy( pImgDx + y * ImgWidth, dx, widthBytesShort );
-        memcpy( pImgDy + y * ImgWidth, dy, widthBytesShort );
-        memcpy( pImgGrad + y * ImgWidth, grad, widthBytesShort );
+        memcpy( pImgDx_ + y * width_, dx, widthBytesShort );
+        memcpy( pImgDy_ + y * width_, dy, widthBytesShort );
+        memcpy( pImgGrad_ + y * width_, g, widthBytesShort );
     }
 }
 #endif
@@ -306,12 +306,12 @@ void GradientOperator::Compute_SSE( const uchar *pImg )
 #ifdef OpenCV
 void GradientOperator::Compute_OpenCV( const uchar *pImg )
 {
-    Mat Img( ImgHeight, ImgWidth, CV_8UC1 );
-    memcpy( Img.data, pImg, ImgHeight * ImgWidth );
+    Mat Img( height_, width_, CV_8UC1 );
+    memcpy( Img.data, pImg, height_ * width_ );
 
-    Mat ImgDx( ImgHeight, ImgWidth, CV_16SC1, pImgDx );
-    Mat ImgDy( ImgHeight, ImgWidth, CV_16SC1, pImgDy );
-    Mat ImgGrad( ImgHeight, ImgWidth, CV_16SC1, pImgGrad );
+    Mat ImgDx( height_, width_, CV_16SC1, pImgDx_ );
+    Mat ImgDy( height_, width_, CV_16SC1, pImgDy_ );
+    Mat ImgGrad( height_, width_, CV_16SC1, pImgGrad_ );
 
     cv::Sobel( Img, ImgDx, CV_16SC1, 1, 0 );
     cv::Sobel( Img, ImgDy, CV_16SC1, 0, 1 );
@@ -328,42 +328,42 @@ void GradientOperator::Compute_OpenCV( const uchar *pImg )
 #ifdef Common
 void GradientOperator::Compute_Common( const uchar *pImg )
 {
-    const int widthBytesShort = ImgWidth * sizeof( short );
+    const int widthBytesShort = width_ * sizeof( short );
 
-    uchar *up = raw;
-    uchar *mid = raw + ImgWidth;
-    uchar *down = raw + ImgWidth * 2;
+    uchar *u = raw_;
+    uchar *m = raw_ + width_;
+    uchar *d = raw_ + width_ * 2;
 
-    short *dx = dx_dy_grad;
-    short *dy = dx_dy_grad + ImgWidth;
-    short *grad = dx_dy_grad + ImgWidth * 2;
+    short *dx = dx_dy_grad_;
+    short *dy = dx_dy_grad_ + width_;
+    short *g = dx_dy_grad_ + width_ * 2;
 
-    for ( int y = 1; y < ImgHeight - 1; ++y )
+    for ( int y = 1; y < height_ - 1; ++y )
     {
         if ( y == 1 )
         {
-            memcpy( raw, pImg, ImgWidth * 3 );
+            memcpy( raw_, pImg, width_ * 3 );
         }
         else
         {
-            uchar *temp = up;
-            up = mid;
-            mid = down;
-            down = temp;
+            uchar *temp = u;
+            u = m;
+            m = d;
+            d = temp;
 
-            memcpy( down, pImg + ImgWidth * ( y + 1 ), ImgWidth );
+            memcpy( d, pImg + width_ * ( y + 1 ), width_ );
         }
 
-        for ( int x = 1; x < ImgWidth - 1; ++x )
+        for ( int x = 1; x < width_ - 1; ++x )
         {
-            dx[x] = up[x + 1] - up[x - 1] + ( mid[x + 1] - mid[x - 1] ) * 2 + down[x + 1] - down[x - 1];
-            dy[x] = down[x - 1] + down[x + 1] + ( down[x] - up[x] ) * 2 - up[x - 1] - up[x + 1];
-            grad[x] = ( Abs( dx[x] ) + Abs( dy[x] ) ) >> 2;
+            dx[x] = u[x + 1] - u[x - 1] + ( m[x + 1] - m[x - 1] ) * 2 + d[x + 1] - d[x - 1];
+            dy[x] = d[x - 1] + d[x + 1] + ( d[x] - u[x] ) * 2 - u[x - 1] - u[x + 1];
+            g[x] = ( Abs( dx[x] ) + Abs( dy[x] ) ) >> 2;
         }
 
-        memcpy( pImgDx + y * ImgWidth, dx, widthBytesShort );
-        memcpy( pImgDy + y * ImgWidth, dy, widthBytesShort );
-        memcpy( pImgGrad + y * ImgWidth, grad, widthBytesShort );
+        memcpy( pImgDx_ + y * width_, dx, widthBytesShort );
+        memcpy( pImgDy_ + y * width_, dy, widthBytesShort );
+        memcpy( pImgGrad_ + y * width_, g, widthBytesShort );
     }
 }
 #endif
--- include/Utility.hpp
+++ include/Utility.hpp
@@ -7,10 +7,9 @@
 #include <opencv2/core/utility.hpp>
 #include <opencv2/core/types_c.h>
 #include <opencv2/core/core_c.h>
-
-#include <iostream>
 #include <opencv2/imgproc.hpp>
 
+#include <iostream>
 #include <string>
 
 
